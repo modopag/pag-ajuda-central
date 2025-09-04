8a96fa7a7829abb61d0444bab8d228377876581d
root <root@ajuda.modopag.com.br>
2025-09-04 12:03:45 +0000

    fix: clean remaining merge markers in render.tsx

--- src/server/render.tsx
+++ src/server/render.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-<<<<<<< HEAD
-#import { HelmetProvider } from 'react-helmet-async';
-=======
+import { HelmetProvider } from 'react-helmet-async';
 // import { HelmetProvider } from 'react-helmet-async';
->>>>>>> 04e9ab1f742a3c80e3aa975b01758bc4c5a05d67
 import App from '../App';
 
 export interface SSRContext {
@@ -25,27 +22,21 @@ export function renderAppToString(context: SSRContext): string {
     },
   });
 
-<<<<<<< HEAD
   #const helmetContext = {};
 
   const app = (
     #<HelmetProvider context={helmetContext}>
-=======
   // const helmetContext = {};
 
   const app = (
     // <HelmetProvider context={helmetContext}>
->>>>>>> 04e9ab1f742a3c80e3aa975b01758bc4c5a05d67
       <QueryClientProvider client={queryClient}>
         <StaticRouter location={context.path}>
           <App ssrData={context.data} />
         </StaticRouter>
       </QueryClientProvider>
-<<<<<<< HEAD
     #</HelmetProvider>
-=======
     // </HelmetProvider>
->>>>>>> 04e9ab1f742a3c80e3aa975b01758bc4c5a05d67
   );
 
   return renderToString(app);
